302f67ae53c685cb36fb0773d4b49471e7df76cd
Nils <imdahlnils@gmail.com>
2020-01-26 20:35:00 +0100

    no message

--- src/pgdp/threads/ChatClient.java
+++ src/pgdp/threads/ChatClient.java
@@ -1,5 +1,46 @@
 package pgdp.threads;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatClient {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Write the ChatServer you would like to connect to.");
+        Scanner clientInput = new Scanner(System.in);
+        String input = clientInput.nextLine();
+        if(input.equals("java ChatClient")){
+            try {
+                Socket client = new Socket("localhost", 3000);
+                System.out.println("Enter UserName:");
+                String userName = clientInput.nextLine();
+                System.out.print("Hello " + userName + "! Welcome to the Chatroom. Instructions:\n" +
+                        "1. Simply type the message to send broadcast to all active clients.\n" +
+                        "2. Type '@username<space>yourmessage' without quotes to send message directly to desired client.\n" +
+                        "3. Type 'WHOIS' without quotes to see list of active clients.\n" +
+                        "4. Type 'LOGOUT without quotes to log off.\n"+
+                        "5. Type 'PENGU' without quotes to request a random penguin fact.\n");
+                PrintWriter writer = new PrintWriter(
+                        client.getOutputStream(), true);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                writer.println(userName);
+                System.out.println(reader.readLine());
+                while(true){
+                    String msg = reader.readLine();
+                    if(msg.equals("LOGOUT")){
+                        client.close();
+                        break;
+                    }else
+                        System.out.println(msg);
+                }
+
+            } catch (IOException e){
+                System.out.println("Connection failed.");
+            }
+        }
+    }
 }
--- src/pgdp/threads/ChatServer.java
+++ src/pgdp/threads/ChatServer.java
@@ -1,6 +1,99 @@
 package pgdp.threads;
 
-public class ChatServer {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ChatServer{
+    private List<Socket> clients;
+    private ServerSocket serverSocket;
+    ObjectInputStream inputStream;
+    ObjectOutputStream outputStream;
+
+    public ChatServer(ServerSocket serverSocket, List<Socket> clients){
+        this.serverSocket = serverSocket;
+        this.clients = clients;
+    }
+    public void waitForConnections(){
+        System.out.println("Waiting to start Server...");
+        Scanner openServerReq = new Scanner(System.in);
+        String s = openServerReq.nextLine();
+        if (s.length() < 15) {
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.print("Input was too small.");
+                return;
+            }
+        }String firstPart = s.substring(0, 15);
+        if (!s.equals("java ChatServer") && firstPart.equals("java ChatServer")) {
+            try {
+                if (s.charAt(15) != ' ')
+                    throw new IllegalArgumentException();
+                int port = Integer.parseInt(s.substring(16));
+                ServerSocket serverSocket = new ServerSocket(port);
+                int count = 0;
+                while(count < 50){
+                    System.out.println("Server waiting on port " + port +"...");
+                    serverSocket.accept();
+                    count++;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Port must be a number.");
+            } catch (IllegalArgumentException e){
+                System.out.print("Input must be of Form java ChatServer <port>");
+            } catch(IndexOutOfBoundsException e) {
+                System.out.println("Illegal input.");
+            } catch (IOException e){
+                System.out.println("Failed to start server, try again.");
+            }
+        }
+        else if (s.equals("java ChatServer")) {
+            try {
+                serverSocket = new ServerSocket(3000);
+                int count = 0;
+                while (count < 50) {
+                    System.out.println("Server is waiting on port 3000...");
+                    Socket socket = serverSocket.accept();
+                    clients.add(socket);
+                    count++;
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                    String username = reader.readLine();
+                    System.out.println("*** " + username + " has joined the Chatroom. ***");
+                    writer.println("*** "+ username +" has joined the ChatRoom. ***");
+                    sendToAll("Hello from Server!");
+                }
+            } catch (IOException e) {
+                System.out.print("Failed to start Server, try again.");
+            }
+        } else {
+            throw new IllegalArgumentException("Input was invalid.");
+        }
+    }
+    public void sendToAll(String msg){
+            for (Socket s : clients) {
+                try {
+                    PrintWriter writer = new PrintWriter(s.getOutputStream(), true);
+                    writer.println(msg);
+                }catch (IOException e){
+                    System.out.println("Couldnt get Outputstream.");
+                }
+            }
+        }
+    public static void main(String[] args) {
+        try {
+            ChatServer chatServer = new ChatServer(new ServerSocket(), new ArrayList<>());
+            chatServer.waitForConnections();
+        } catch (IOException e) {
+            System.out.println("IO-Exception.");
+        }
+
+    }
 
 }
     
